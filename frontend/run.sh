45a5b6dd840c6fd017f865e06c32aefbf094565a
João Gabriel De Lima <joao.lima@dbdiagnosticos.com.br>
2022-06-07 21:12:30 -0300

    Correção base.robot

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,2 +1,2 @@
 robot -l NONE -o NONE -r NONE tasks/Delorean.robot
-robot -x xunit.xml -d ./logs -v BROWSER:chromium -v HEADLESS:True -e smoke tests
+robot -x xunit.xml -d ./logs -v BROWSER:chromium -v HEADLESS:True tests
